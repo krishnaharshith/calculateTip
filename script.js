58cbb01ae47ef7fb44c1031810ca4b6605ac1cfd
krishnaharshith <108270779+krishnaharshith@users.noreply.github.com>
2024-09-02 15:02:12 +0530

    updated total functionality.

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const calculatePrice = function () {
-  let element = document.querySelector(".total-div");
-  element.classList.remove("hide");
-};
+// const calculatePrice = function () {
+//   let element = document.querySelector(".total-div");
+//   element.classList.remove("hide");
+// };
+
+document.querySelector(".button").addEventListener("click", function () {
+  let amount = Number(document.querySelector("#amount").value);
+  let tip = Number(document.querySelector("#tip").value);
+  console.log(amount, tip);
+  let total = amount + (tip * amount) / 100;
+  console.log(total);
+  document.querySelector(".price").textContent = total + 0.0;
+  document.querySelector(".total-div").classList.remove("hide");
+});
